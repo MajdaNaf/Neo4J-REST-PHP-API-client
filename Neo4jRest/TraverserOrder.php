c29846619aaa7f6ac697b19af2a2e7366776cdfc
tchaffee <tc@toddbiz.com>
2011-08-23 17:02:42 +0100

    Started coding for traversals

---
+++ Neo4jRest/TraverserOrder.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Defines a traversal order as used by the traversal framework.
+ * 
+ * @author tchaffee
+ * @package Neo4jRest
+ * 
+ */
+
+namespace Neo4jRest;
+
+class TraverserOrder {
+	const BREADTH_FIRST = 'breadth first';
+	const DEPTH_FIRST = 'depth first';
+		
+}
---
+++ Neo4jRest/TraverserReturnFilter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Defines a traversal order as used by the traversal framework.
+ * 
+ * @author tchaffee
+ * @package Neo4jRest
+ * 
+ */
+
+namespace Neo4jRest;
+
+class TraverserReturnFilter {
+	const ALL = 'all';
+	const ALL_BUT_START_NODE = 'all but start node';
+		
+}
